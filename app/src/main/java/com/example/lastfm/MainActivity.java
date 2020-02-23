c37e0db46d323fb5fda860af77759fa9188ac6bc
Dmitry Tolmachev <59831348+tolmadima@users.noreply.github.com>
2020-02-23 16:06:51 +0300

    Trying to create Adapter and realisation of this adapter

--- app/src/main/java/com/example/lastfm/MainActivity.java
+++ app/src/main/java/com/example/lastfm/MainActivity.java
@@ -17,6 +17,37 @@ public class MainActivity extends AppCompatActivity {
 
         RecyclerView recyclerView;
         recyclerView = findViewById(R.id.RecyclerView);
+        recyclerView.adapter = Adapter(generateFakeNews())
+        rectclerView.layoutManager = LinearLayoutManager(this)
     }
 
-}
+    private fun generateFakeNews(): List<String>{
+        val values = mutableListOf<String>()
+                for(i in 0..100){
+                    values.add("$i element")
+        }
+        return values
+    }
+
+
+
+    class Adapter(private val values: List<String>): RecyclerView.Adapter<Adapter.ViewHolder>() {
+        @Override fun getItemCount()= values.size
+
+        @Override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int){
+            val itemView = LaoutInflater.from(parent?.context).inflate(R.layout.list_item_view, parent, false)
+                    return ViewHolder(itemView)
+        }
+
+        @Override fun onBindViewHolder(holder: ViewHolder?, position Int){
+            holder?.textView?.text = values[position]
+        }
+
+        Class ViewHolder(itemView: View?) : RecyclerView.ViewHolder(itemView){
+            var textView: TextView? = null
+                init{
+                textView = itemView?.findViewById(R.id.text_list_item)
+            }
+            }
+        }
+    }
