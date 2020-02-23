0a840b63f2a44ca1bab77fbc8125c117e4456eb4
Dmitry Tolmachev <59831348+tolmadima@users.noreply.github.com>
2020-02-23 15:27:45 +0300

    Initial commit

---
+++ app/src/main/java/com/example/lastfm/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.lastfm;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import android.renderscript.Sampler;
+import android.widget.LinearLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        RecyclerView recyclerView;
+        recyclerView = findViewById(R.id.RecyclerView);
+    }
+
+
+}
