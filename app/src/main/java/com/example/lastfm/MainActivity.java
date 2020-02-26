97d4c15a1ae0f6c0ad7fc941bb9acb6f64462908
Dmitry Tolmachev <59831348+tolmadima@users.noreply.github.com>
2020-02-26 08:08:35 +0300

    initiated adapter in MainActivity
    Added RecyclerView initiation, Adapter

--- app/src/main/java/com/example/lastfm/MainActivity.java
+++ app/src/main/java/com/example/lastfm/MainActivity.java
@@ -5,22 +5,61 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
-import android.renderscript.Sampler;
-import android.widget.LinearLayout;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
+
+import com.example.lastfm.NewsData;
+import com.example.lastfm.NewsAdapter;
+
+
+import java.util.Arrays;
+import java.util.Collection;
 
 public class MainActivity extends AppCompatActivity {
-    private RecyclerView recyclerView;
-    private RecyclerView.Adapter mAdapter;
-    private RecyclerView.LayoutManager layoutManager;
+    private ImageView newsImage;
+    private TextView textHeader;
+    private TextView textDescription;
+
+
+    private RecyclerView newsRecyclerView;
+    private NewsAdapter newsAdapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        findViewById(R.id.RecyclerView);
 
-        layoutManager = new LinearLayoutManager(this);
-        recyclerView.setLayoutManager(layoutManager);
 
+        initRecyclerView();
+
+        loadNews();
     }
+
+        private void loadNews(){
+            Collection<NewsData> news = getNews();
+            newsAdapter.setItems(news);
+        }
+
+
+        private Collection<NewsData> getNews(){
+           return Arrays.asList(
+                   new NewsData("RandomNewsTitle","https://www.w3schools.com/w3images/lights.jpg","LUL")
+           );
+        }
+    private void initRecyclerView() {
+        newsRecyclerView = findViewById(R.id.newsRecyclerView);
+        newsRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        newsAdapter = new NewsAdapter();
+        newsRecyclerView.setAdapter(newsAdapter);
     }
+
+
+
+
+
+
+
+}
