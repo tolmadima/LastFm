5ecc7ead3b8a48709f9d2b731582cea590fc7d03
Dmitry Tolmachev <59831348+tolmadima@users.noreply.github.com>
2020-02-26 08:04:09 +0300

    Cosmetical fix
    Changed name of third variable

--- app/src/main/java/com/example/lastfm/NewsData.java
+++ app/src/main/java/com/example/lastfm/NewsData.java
@@ -1,18 +1,18 @@
 package com.example.lastfm;
 
 public class NewsData {
-    String Header;
-    String Image;
-    String Text;
+   private String Header;
+   private String Image;
+   private String Description;
 
 
-    public NewsData(String Header, String Image, String Text){
+    NewsData(String Header, String Image, String Text){
         this.Header = Header;
         this.Image = Image;
-        this.Text = Text;
+        this.Description = Text;
     }
 
     public String getHeader(){return Header;}
     public String getImage(){return Image;}
-    public String getText(){return Text;}
+    public String getDescription(){return Description;}
 }
