b8303151439f142815029c580acf7551fd9d4cb6
Dmitry Tolmachev <59831348+tolmadima@users.noreply.github.com>
2020-02-26 20:08:39 +0300

    Filling Array with hardcoded news

--- app/src/main/java/com/example/lastfm/MainActivity.java
+++ app/src/main/java/com/example/lastfm/MainActivity.java
@@ -46,7 +46,14 @@ public class MainActivity extends AppCompatActivity {
 
         private Collection<NewsData> getNews(){
            return Arrays.asList(
-                   new NewsData("RandomNewsTitle","https://www.w3schools.com/w3images/lights.jpg","LUL")
+                   new NewsData("RandomNewsTitle","https://www.w3schools.com/w3images/lights.jpg","LUL"),
+                   new NewsData("Тилль Линдеманн показал клип своего нового проекта Na chui",
+                                 "https://i0.wp.com/sova.ponominalu.ru/wp-content/uploads/2020/02/lindemann-scaled.jpg?resize=1920%2C1280&ssl=1",
+                                   "Артист экспериментирует с танцевальной музыкой и психикой фанатов. Тилль Линдеманн продолжает выпускать провокационные видео. В предыдущем ролике «Platz Eins», выпущенном проектом Lindemann, все самые откровенные сцены подверглись цензуре. Однако теперь можно увидеть и полную версию — она доступна на немецком сайте Vizit-X за деньги. Кадры использовались для видео на песню «Till the End», которую Линдеманн выпустил от лица своего нового проекта с понятным каждому русскому человеку названием Na Chui. В нём Тилль экспериментирует с танцевальной музыкой. Если вы не хотите смотреть откровенное видео, трек можно послушать на стримингах"),
+                   new NewsData("Новый альбом группы 'Пошлая Молли'","https://avatars.mds.yandex.net/get-zen_doc/29030/pub_5c37ba580129cd00aaa5dff8_5c37bb4b512ee600aaa230fe/",
+                                   "«Пошлая Мо́лли» — украинская поп-панк-группа, основанная музыкантом из Харькова Кириллом Тимошенко, более известным под псевдонимом Кирилл Бледный. Группа является одним из самых ярких представителей синти-панка на Украине, совмещая поп-панк с электронной музыкой. Лидер группы говорит о «Пошлой Молли» как о собирательном образе испорченной школьницы."),
+                   new NewsData("ZXc","https://www.w3schools.com/w3images/lights.jpg","LUL"),
+                   new NewsData("News","https://www.w3schools.com/w3images/lights.jpg","LUL")
            );
         }
     private void initRecyclerView() {
