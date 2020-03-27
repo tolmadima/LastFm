2751b4311e40b182847f68175814503441fd98c3
Dmitry Tolmachev <59831348+tolmadima@users.noreply.github.com>
2020-03-27 15:47:03 +0300

    Revert "Merge branch 'master' into ArtistScreenActivity"
    This reverts commit ea2a099847dbe8a7b00a0cb03dbdfd5d9d085f2b, reversing
    changes made to f08ad9394fbde1ce45519ffc368f23887f391167.

--- app/src/main/java/com/example/lastfm/MainActivity.java
+++ app/src/main/java/com/example/lastfm/MainActivity.java
@@ -3,27 +3,19 @@ package com.example.lastfm;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.os.Bundle;
-import android.util.Log;
+
 import com.google.gson.JsonObject;
-import android.annotation.SuppressLint;
-import android.os.AsyncTask;
-import android.os.Bundle;
-import android.util.Log;
-import com.google.gson.Gson;
-import com.google.gson.JsonArray;
-import com.google.gson.JsonObject;
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
+
 import java.util.ArrayList;
 import java.util.List;
 import retrofit2.Call;
+import retrofit2.Callback;
 import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
