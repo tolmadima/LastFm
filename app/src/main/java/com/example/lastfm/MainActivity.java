4a26131a4bf90cc45d3219b03923ab04b31c62d2
Dmitry Tolmachev <59831348+tolmadima@users.noreply.github.com>
2020-04-10 15:05:59 +0300

    Revert "Revert "Added 2 fragments on MainActivity, remade onClick method to make request from starting new activity""
    This reverts commit 43ee425816428c8a2b777e7acda138773a634bfa.

--- app/src/main/java/com/example/lastfm/MainActivity.java
+++ app/src/main/java/com/example/lastfm/MainActivity.java
@@ -1,12 +1,25 @@
 package com.example.lastfm;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import com.example.lastfm.ArtistInfo.ArtistData;
+import com.example.lastfm.ArtistInfo.ArtistInfo;
+import com.example.lastfm.ArtistInfo.Bio;
+import com.example.lastfm.ArtistInfo.Image;
+import com.example.lastfm.ArtistInfo.Stats;
+import com.squareup.picasso.Picasso;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -15,10 +28,18 @@ import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.disposables.Disposable;
 import io.reactivex.schedulers.Schedulers;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends FragmentActivity {
     private final int NUMBER_OF_ARTISTS = 40;
     private List<Artist> requestedArtists = new ArrayList<>();
     private SwipeRefreshLayout mSwipeRefreshLayout;
+    private TextView tvNameView;
+    private TextView tvPlayCount;
+    private ImageView artistImage;
+    private TextView tvArtistBio;
+    //Вызывается массив картинок размер которых варьируется от 0-4
+    //0 - самое маленькое разрешение
+    //4 - самое большое разрешение
+    private static final int PICTURE_SIZE = 3;
 
     ArtistAdapter artistsAdapter = new ArtistAdapter(this::onArtistClick);
 
@@ -43,25 +64,30 @@ public class MainActivity extends AppCompatActivity {
                 .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
                 .subscribe(new SingleObserver<List<Artist>>() {
-                        @Override
-                        public void onSubscribe(Disposable d) {
-                        }
+                    @Override
+                    public void onSubscribe(Disposable d) {
+                    }
 
-                        @Override
-                        public void onSuccess(List<Artist> info) {
-                            requestedArtists = info;
-                            showArtists(requestedArtists);
-                            hideRefreshing();
-                        }
+                    @Override
+                    public void onSuccess(List<Artist> info) {
+                        requestedArtists = info;
+                        showArtists(requestedArtists);
+                        hideRefreshing();
+                    }
 
-                        @Override
-                        public void onError(Throwable e) {
-                            e.printStackTrace();
-                            hideRefreshing();
-                            String requestErrorText = getString(R.string.request_error_message);
-                            Toast.makeText(MainActivity.this, requestErrorText, Toast.LENGTH_LONG).show();
-                        }
-                    });
+                    @Override
+                    public void onError(Throwable e) {
+                        e.printStackTrace();
+                        hideRefreshing();
+                        String requestErrorText = getString(R.string.request_error_message);
+                        Toast.makeText(MainActivity.this,
+                                requestErrorText, Toast.LENGTH_LONG).show();
+                    }
+                });
+        tvNameView = findViewById(R.id.artist_info_name);
+        tvPlayCount = findViewById(R.id.artist_info_playcount);
+        artistImage = findViewById(R.id.artist_info_image);
+        tvArtistBio = findViewById(R.id.artist_info_bio);
     }
 
     private void hideRefreshing(){
@@ -73,20 +99,53 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void initRecyclerView() {
-        RecyclerView artistsRecyclerView = findViewById(R.id.rv_artists);
-        artistsRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        RecyclerView rvArtists = findViewById(R.id.rv_artists);
+        rvArtists.setLayoutManager(new LinearLayoutManager(this));
         artistsAdapter = new ArtistAdapter(new ArtistAdapter.OnArtistListener() {
             @Override
             public void onArtistClick(int position) {
                 MainActivity.this.onArtistClick(position);
             }
         });
-        artistsRecyclerView.setAdapter(artistsAdapter);
+        rvArtists.setAdapter(artistsAdapter);
+    }
+
+    public void showInfo(ArtistInfo info){
+        ArtistData data = info.getArtist();
+        String artistName = data.getName();
+        Bio artistBio = data.getBio();
+        Stats artistStat = data.getStats();
+        String playcount = artistStat.getPlaycount();
+        String bio = artistBio.getContent();
+        Image url = data.getImage().get(PICTURE_SIZE);
+        String imageUrl = url.getText();
+        tvNameView.setText(artistName);
+        tvArtistBio.setText(bio);
+        tvPlayCount.setText(playcount);
+        Picasso.get().load(imageUrl).into(artistImage);
+        artistImage.setVisibility(imageUrl != null ? View.VISIBLE : View.GONE);
     }
 
     private void onArtistClick(int position) {
-        Intent intent = new Intent(MainActivity.this,ArtistInfoActivity.class);
-        intent.putExtra("artistName",requestedArtists.get(position).getArtistName());
-        startActivity(intent);
+        LastFMClient client = ServiceGenerator.getInstance().getLastFMClient();
+        client.getArtistInfo(requestedArtists.get(position).getArtistName())
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new SingleObserver<ArtistInfo>() {
+                    @Override
+                    public void onSubscribe(Disposable d) {
+                    }
+
+                    @Override
+                    public void onSuccess(ArtistInfo info) {
+                        showInfo(info);
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+                        String text = getString(R.string.request_error_message);
+                        Toast.makeText(MainActivity.this, text, Toast.LENGTH_LONG).show();
+                    }
+                });
     }
 }
